665426a50964bb029aae08ec64d85b39e2cb97f4
0000 <user7210@proton.me>
2026-06-04 12:24:26 +0200

    Delete config directory

--- config/sway/configs/waybar/scripts/power.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sleep 0.1
-~/.config/sway/scripts/power.sh & disown
--- config/sway/configs/waybar/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-WAYBAR_CFG_DIR="$HOME/.config/hypr/configs/waybar"
-
-sleep 0.1
-
-waybar -c "$WAYBAR_CFG_DIR/config.jsonc" -s "$WAYBAR_CFG_DIR/style.css" &
